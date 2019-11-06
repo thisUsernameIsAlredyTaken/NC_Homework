f0772b319a8a340a0a2ed3a778af0980cdc7e02a
Zhivaev Artem <zhivaev993@gmail.com>
2019-11-06 11:34:39 +0300

    Point Classes

---
+++ Exercises/src/com/example/Point2D.java
@@ -0,0 +1,61 @@
+package com.example;
+
+import java.util.Objects;
+
+public class Point2D {
+
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    public Point2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point2D() {
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float[] getXY() {
+        return new float[]{x, y};
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point2D point2D = (Point2D) o;
+        return Float.compare(point2D.x, x) == 0 &&
+                Float.compare(point2D.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
---
+++ Exercises/src/com/example/Point3D.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import java.util.Objects;
+
+public class Point3D extends Point2D {
+
+    private float z = 0.0f;
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x, float y, float z) {
+        super.setXY(x, y);
+        this.z = z;
+    }
+
+    public float[] getXYZ() {
+        return new float[]{getX(), getY(), z};
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Point3D point3D = (Point3D) o;
+        return Float.compare(point3D.z, z) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), z);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getX() + ',' + getY() + ',' + z + ')';
+    }
+}
