c38297a597697d726e14a9adff84e20135e8b50b
Zhivaev Artem <zhivaev993@gmail.com>
2019-11-06 11:34:07 +0300

    Person classes added

---
+++ Exercises/src/com/example/Staff.java
@@ -0,0 +1,53 @@
+package com.example;
+
+import java.util.Objects;
+
+public class Staff extends Person {
+
+    private String school;
+    private double pay;
+
+    public Staff(String name, String address, String school, double pay) {
+        super(name, address);
+        this.school = school;
+        this.pay = pay;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public double getPay() {
+        return pay;
+    }
+
+    public void setPay(double pay) {
+        this.pay = pay;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff[" + super.toString() +
+                ",school=" + school +
+                ",pay=" + pay + ']';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Staff staff = (Staff) o;
+        return Double.compare(staff.pay, pay) == 0 &&
+                Objects.equals(school, staff.school);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), school, pay);
+    }
+}
---
+++ Exercises/src/com/example/Student.java
@@ -0,0 +1,65 @@
+package com.example;
+
+import java.util.Objects;
+
+public class Student extends Person {
+
+    private String program;
+    private int year;
+    private double fee;
+
+    public Student(String name, String address, String program, int year, double fee) {
+        super(name, address);
+        this.program = program;
+        this.year = year;
+        this.fee = fee;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    @Override
+    public String toString() {
+        return "Student[" + super.toString() +
+                ",program=" + program +
+                ",year=" + year +
+                ",fee=" + fee + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Student student = (Student) o;
+        return year == student.year &&
+                Double.compare(student.fee, fee) == 0 &&
+                Objects.equals(program, student.program);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), program, year, fee);
+    }
+}
