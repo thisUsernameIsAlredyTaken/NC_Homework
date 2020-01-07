41d5876b2252acdb491944b2e2670b873e2086cf
Zhivaev Artem <zhivaev993@gmail.com>
2020-01-07 16:46:15 +0300

    Added pom.xml and starter class

---
+++ task7/src/com/example/homework/HomeworkApplication.java
@@ -0,0 +1,14 @@
+package com.example.homework;
+
+import com.example.homework.entity.Customer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkApplication.class, args);
+    }
+
+}
